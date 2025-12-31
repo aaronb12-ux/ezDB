ab294bfe96b9159b84a30ff77026b6bd69fde3d2
aaronb12-ux <afairster03@gmail.com>
2025-12-31 14:03:01 -0800

    adjusted b+tree def and maketree function

--- btree/btree.go
+++ btree/btree.go
@@ -1,20 +1,25 @@
 package btree
 
+
 type BTree struct {
-	Root *Node
-	Order int //max number of the children the INTERNAL NODE can have. and defines the number of keys an internal node can have which is Order - 1
-	Size int //number of keys
+	Root   *Node 
+	Order  int  
+	Size   int  
+	Height int  
 }
 
+
 type Key struct {
-	Key int
-	Value []byte
+	Key   int   
+	Value []byte 
 }
 
 type Node struct {
-	Data *[]Key //data is an array of Keys (only for leaf nodes)
-	Children *[]int //for internal nodes. they have children
-	Next *int //only for leaf nodes
-	Keys *[]int //for internal nodes. internal nodes have an array of keys which are used for the searching
+	IsLeaf   bool    
+	Keys     []int   
+	Children []*Node
+	Data     []Key   
+	Next     *Node  
+	Parent   *Node
 }
 
